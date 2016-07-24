484f84da5c2d22be40fd5c304030674dedfcca14
Andrei Pozniak <krucios@mail.ru>
2016-07-25 00:50:27 +0300

    [~] changed priority of interrupts

--- Modules/Time/timer.c
+++ Modules/Time/timer.c
@@ -14,6 +14,7 @@ void timers_init(void) {
 }
 
 void timer_mss1_init(mss_timer_mode_t mode, uint32_t load_value) {
+    NVIC_SetPriority(Timer1_IRQn, 0x80u);
     MSS_TIM1_init(mode);
     MSS_TIM1_load_background(load_value);
 }
@@ -29,6 +30,7 @@ void timer_mss1_stop(void) {
 }
 
 void timer_mss2_init(mss_timer_mode_t mode, uint32_t load_value) {
+    NVIC_SetPriority(Timer2_IRQn, 0x40u);
     MSS_TIM2_init(mode);
     MSS_TIM2_load_background(load_value);
 }
@@ -48,6 +50,7 @@ void timer_sys_init(uint32_t load_value) {
      * Initialize the system tick for 10mS operation or 1 tick every 100th of
      * a second and also make sure it is lower priority than the I2C IRQs.
      */
-    NVIC_SetPriority(SysTick_IRQn, 0xFFu); /* Lowest possible priority */
+    NVIC_SetPriority(FabricIrq0_IRQn, 0x10u);
+    NVIC_SetPriority(SysTick_IRQn, 0x11u);
     SysTick_Config(load_value);
 }
