4525ee7d9b3ad65d186fe148cda370ef7bf344fa
Андрей <krucios@mail.ru>
2017-03-30 15:55:09 +0300

    [~] added interface to comunicate with US HW module

--- Modules/UART/US/us.c
+++ Modules/UART/US/us.c
@@ -5,4 +5,22 @@
  *      Author: Andrei.PAzniak (krucios@mail.ru)
  */
 
+#include "us.h"
 
+UART_instance_t g_us;
+
+inline void us_init(addr_t base_addr, uint16_t baud_val) {
+    UART_init(&g_us, base_addr, baud_val, (DATA_8_BITS | NO_PARITY));
+}
+
+inline size_t us_get_rx(uint8_t* rx_buffer, size_t buffer_size) {
+    return (UART_get_rx(&g_us, rx_buffer, buffer_size));
+}
+
+inline void us_send(const uint8_t* tx_buf, size_t size) {
+    UART_send(&g_us, tx_buf, size);
+}
+
+inline void us_polled_tx_string(const uint8_t* str) {
+    UART_polled_tx_string(&g_us, str);
+}
--- Modules/UART/US/us.h
+++ Modules/UART/US/us.h
@@ -8,6 +8,13 @@
 #ifndef MODULES_UART_US_US_H_
 #define MODULES_UART_US_US_H_
 
+#include <drivers/CoreUARTapb/core_uart_apb.h>
 
+extern UART_instance_t g_us;
+
+void     us_init(addr_t base_addr, uint16_t baud_val);
+size_t   us_get_rx(uint8_t* rx_buffer, size_t buffer_size);
+void     us_send(const uint8_t* tx_buf, size_t size);
+void     us_polled_tx_string(const uint8_t* str);
 
 #endif /* MODULES_UART_US_US_H_ */
